7f6a0752b14d4872456a4dda9425b9cfe35944a3
verybigorange <449732828@qq.com>
2017-09-10 21:44:56 +0800

    first commit

---
+++ src/renderer/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'vue_photoshop'
+  }
+</script>
+
+<style lang="scss">
+  html,body{
+    width: 100%;
+    height:100%;
+  }
+  /* CSS */
+   body{
+    #app{
+      background: red;
+      height: 2vh;
+    }
+  }
+</style>
---
+++ src/renderer/views/LandingPage.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="wrapper">
+      11122
+  </div>
+</template>
+
+<script>
+  
+
+export default {
+  
+}
+</script>
+
+<style >
+ 
+ 
+</style>
