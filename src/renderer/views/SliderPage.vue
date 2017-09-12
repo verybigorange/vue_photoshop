73a9750892aa6b44df39908b5fb7aabe1cb6a68d
周诚 <zhoucheng1993@foxmail.com>
2017-09-12 22:51:50 +0800

    将页面数据分离到data里

--- src/renderer/views/SliderPage.vue
+++ src/renderer/views/SliderPage.vue
@@ -1,61 +1,18 @@
 <template>
 	<div class="slider">
 		<ul>
-			<li v-for="(item,index) in tools" :key="item.showTool.text" class="iconfoot" :class="item.showTool.iconClass" :title="item.showTool.text"></li>
+			<li v-for="(item,index) in tools" :key="item.showTool.text" class="iconfont" :class="item.showTool.iconClass" :title="item.showTool.text"></li>
 		</ul>
-		<a class="iconfoot icon-move"></a>
 	</div>
 </template>
 
 <script>
+import tools from "./data/tools"
+
 export default {
 	data() {
 		return {
-			tools: [
-				{
-					showTool: {
-						text: "移动工具",
-						iconClass: "icon-move"
-					},
-					shortcutKey: "V",
-					children: [
-						{
-							text: "移动工具",
-							iconClass: "icon-move"
-						},
-						{
-							text: "画板工具",
-							iconClass: "icon-huabangongjushouxinggongju"
-						}
-					]
-				},
-				{
-					showTool: {
-						text: "矩形选框工具",
-						iconClass: ""
-					},
-					shortcutKey: "M",
-					children: [
-						{
-							text: "矩形选框工具",
-							iconClass: ""
-						},
-						{
-							text: "椭圆选框工具",
-							iconClass: ""
-						},
-						{
-							text: "单行选框工具",
-							iconClass: ""
-						},
-						{
-							text: "单列选框工具",
-							iconClass: ""
-						}
-					]
-				},
-
-			]
+			tools: tools
 		}
 	}
 }
---
+++ src/renderer/views/data/tools.js
@@ -0,0 +1,371 @@
+export default [{
+        showTool: {
+            text: "移动工具",
+            iconClass: "icon-move"
+        },
+        shortcutKey: "V",
+        children: [{
+                text: "移动工具",
+                iconClass: "icon-move"
+            },
+            {
+                text: "画板工具",
+                iconClass: "icon-huabangongjushouxinggongju"
+            }
+        ]
+    },
+    {
+        showTool: {
+            text: "矩形选框工具",
+            iconClass: "icon-kuangxuan"
+        },
+        shortcutKey: "M",
+        children: [{
+                text: "矩形选框工具",
+                iconClass: "icon-kuangxuan"
+            },
+            {
+                text: "椭圆选框工具",
+                iconClass: ""
+            },
+            {
+                text: "单行选框工具",
+                iconClass: ""
+            },
+            {
+                text: "单列选框工具",
+                iconClass: ""
+            }
+        ]
+    },
+    {
+        showTool: {
+            text: "套索工具",
+            iconClass: ""
+        },
+        shortcutKey: "L",
+        children: [{
+                text: "套索工具",
+                iconClass: ""
+            },
+            {
+                text: "多边形套索工具",
+                iconClass: ""
+            },
+            {
+                text: "磁性套索工具",
+                iconClass: ""
+            }
+        ]
+    },
+    {
+        showTool: {
+            text: "快速选择工具",
+            iconClass: ""
+        },
+        shortcutKey: "W",
+        children: [{
+                text: "快速选择工具",
+                iconClass: ""
+            },
+            {
+                text: "魔棒工具",
+                iconClass: ""
+            },
+        ]
+    },
+    {
+        showTool: {
+            text: "裁剪工具",
+            iconClass: ""
+        },
+        shortcutKey: "C",
+        children: [{
+                text: "裁剪工具",
+                iconClass: ""
+            },
+            {
+                text: "透视裁剪工具",
+                iconClass: ""
+            },
+            {
+                text: "切片工具",
+                iconClass: ""
+            },
+            {
+                text: "切片选择工具",
+                iconClass: ""
+            },
+        ]
+    },
+    {
+        showTool: {
+            text: "吸管工具",
+            iconClass: ""
+        },
+        shortcutKey: "I",
+        children: [{
+                text: "吸管工具",
+                iconClass: ""
+            },
+            {
+                text: "标尺工具",
+                iconClass: ""
+            },
+        ]
+    },
+    {
+        showTool: {
+            text: "污点修复画笔工具",
+            iconClass: ""
+        },
+        shortcutKey: "J",
+        children: [{
+                text: "污点修复画笔工具",
+                iconClass: ""
+            },
+            {
+                text: "修复画笔工具",
+                iconClass: ""
+            },
+            {
+                text: "修补工具",
+                iconClass: ""
+            },
+        ]
+    },
+    {
+        showTool: {
+            text: "画笔工具",
+            iconClass: ""
+        },
+        shortcutKey: "B",
+        children: [{
+                text: "画笔工具",
+                iconClass: ""
+            },
+            {
+                text: "铅笔工具",
+                iconClass: ""
+            },
+        ]
+    },
+    {
+        showTool: {
+            text: "仿制图章工具",
+            iconClass: ""
+        },
+        shortcutKey: "S",
+        children: [{
+                text: "仿制图章工具",
+                iconClass: ""
+            },
+            {
+                text: "图案图章工具",
+                iconClass: ""
+            },
+        ]
+    },
+    {
+        showTool: {
+            text: "历史记录画笔工具",
+            iconClass: ""
+        },
+        shortcutKey: "Y",
+        children: [{
+                text: "历史记录画笔工具",
+                iconClass: ""
+            },
+            {
+                text: "历史记录艺术画笔工具",
+                iconClass: ""
+            },
+        ]
+    },
+    {
+        showTool: {
+            text: "橡皮擦工具",
+            iconClass: ""
+        },
+        shortcutKey: "E",
+        children: [{
+                text: "橡皮擦工具",
+                iconClass: ""
+            },
+            {
+                text: "背景橡皮擦工具",
+                iconClass: ""
+            },
+            {
+                text: "魔术橡皮擦工具",
+                iconClass: ""
+            },
+        ]
+    },
+    {
+        showTool: {
+            text: "渐变工具",
+            iconClass: ""
+        },
+        shortcutKey: "G",
+        children: [{
+                text: "渐变工具",
+                iconClass: ""
+            },
+            {
+                text: "油漆桶工具",
+                iconClass: ""
+            },
+        ]
+    },
+    {
+        showTool: {
+            text: "模糊工具",
+            iconClass: ""
+        },
+        shortcutKey: null,
+        children: [{
+                text: "模糊工具",
+                iconClass: ""
+            },
+            {
+                text: "锐化工具",
+                iconClass: ""
+            },
+            {
+                text: "涂抹工具",
+                iconClass: ""
+            },
+        ]
+    },
+    {
+        showTool: {
+            text: "减淡工具",
+            iconClass: ""
+        },
+        shortcutKey: "O",
+        children: [{
+                text: "减淡工具",
+                iconClass: ""
+            },
+            {
+                text: "加深工具",
+                iconClass: ""
+            },
+            {
+                text: "海绵工具",
+                iconClass: ""
+            },
+        ]
+    },
+    {
+        showTool: {
+            text: "钢笔工具",
+            iconClass: ""
+        },
+        shortcutKey: "P",
+        children: [{
+                text: "钢笔工具",
+                iconClass: ""
+            },
+            {
+                text: "自由钢笔工具",
+                iconClass: ""
+            },
+            {
+                text: "添加锚点工具",
+                iconClass: ""
+            },
+            {
+                text: "删除锚点工具",
+                iconClass: ""
+            },
+        ]
+    },
+    {
+        showTool: {
+            text: "横排文字工具",
+            iconClass: ""
+        },
+        shortcutKey: "T",
+        children: [{
+                text: "横排文字工具",
+                iconClass: ""
+            },
+            {
+                text: "直排文字工具",
+                iconClass: ""
+            },
+        ]
+    },
+    {
+        showTool: {
+            text: "路径选择工具",
+            iconClass: ""
+        },
+        shortcutKey: "A",
+        children: [{
+                text: "路径选择工具",
+                iconClass: ""
+            },
+            {
+                text: "直接选择工具",
+                iconClass: ""
+            },
+        ]
+    },
+    {
+        showTool: {
+            text: "矩形工具",
+            iconClass: ""
+        },
+        shortcutKey: "U",
+        children: [{
+                text: "矩形工具",
+                iconClass: ""
+            },
+            {
+                text: "圆角矩形工具",
+                iconClass: ""
+            },
+            {
+                text: "椭圆工具",
+                iconClass: ""
+            },
+            {
+                text: "多边形工具",
+                iconClass: ""
+            },
+            {
+                text: "直线工具",
+                iconClass: ""
+            },
+            {
+                text: "自定形状工具",
+                iconClass: ""
+            },
+        ]
+    },
+    {
+        showTool: {
+            text: "抓手工具",
+            iconClass: ""
+        },
+        shortcutKey: "H",
+        children: [{
+            text: "抓手工具",
+            iconClass: ""
+        }, ]
+    },
+    {
+        showTool: {
+            text: "缩放工具",
+            iconClass: ""
+        },
+        shortcutKey: "Z",
+        children: [{
+            text: "缩放工具",
+            iconClass: ""
+        }, ]
+    },
+]
